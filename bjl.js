8e944dc0cb9b6d374059d61df953373cf460e4c2
August <raul@bollingerinternational.com>
2015-12-27 09:52:53 -0800

    Added a namespace object

--- bjl.js
+++ bjl.js
@@ -3,7 +3,9 @@
  * BJS.js (Bollinger JavaScript Library) v0.0.1
  *
  ************************************************/
-var cloneObject = function (src, dest) {
+var bjl = bjl || {};
+
+bjl.cloneObject = function (src, dest) {
     for(var attr in src.prototype) {
         dest.prototype[attr] = src.prototype[attr];
     }    
