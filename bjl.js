1d97277d1e23c7df065c5a3fba9e5361a7417464
August <raul@bollingerinternational.com>
2015-12-27 09:34:18 -0800

    Added description and version

--- bjl.js
+++ bjl.js
@@ -1,3 +1,8 @@
+/*************************************************
+ *
+ * BJS.js (Bollinger JavaScript Library) v0.0.1
+ *
+ ************************************************/
 var cloneObject = function (src, dest) {
     for(var attr in src.prototype) {
         dest.prototype[attr] = src.prototype[attr];
